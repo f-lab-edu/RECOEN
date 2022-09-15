fb6087f9a89fa75a866846ee2609bddc8b6bf6a9
yechan <noy3928@naver.com>
2022-09-15 15:38:10 +0900

    refactor : remove jest-next-dynamic

--- src/components/molecules/TextEditor/index.test.tsx
+++ src/components/molecules/TextEditor/index.test.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { TextEditor } from './index';
-import preloadAll from 'jest-next-dynamic';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('<TextEditor />', () => {
   it('renders TextEditor component correctly', async () => {
-    await preloadAll();
-
     const onChange = jest.fn();
 
     const { container } = render(<TextEditor onChange={onChange} />);
