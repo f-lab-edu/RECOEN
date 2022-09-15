b86ce9d6a89563c95b293aaf8b9588018fa4e12a
yechan <noy3928@naver.com>
2022-09-15 11:10:48 +0900

    feat : wrap original editor with forwarded wrapper and get markdown data

--- src/components/molecules/TextEditor/index.tsx
+++ src/components/molecules/TextEditor/index.tsx
@@ -1,15 +1,38 @@
+/* eslint-disable react/display-name */
+import React, { useEffect, useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 import '@toast-ui/editor/dist/toastui-editor.css';
-import { EditorProps } from '@toast-ui/react-editor';
+import { Editor as EditorType, EditorProps } from '@toast-ui/react-editor';
+import { EditorPropsWithForwardedProps } from './EditorWrapper';
 
-const Editor = dynamic<EditorProps>(
-  () => import('@toast-ui/react-editor').then((m) => m.Editor),
-  { ssr: false },
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+const Editor = dynamic<EditorPropsWithForwardedProps>(
+  () => import('./EditorWrapper'),
+  {
+    ssr: false,
+  },
 );
 
+const EditorWithForwardedRef = React.forwardRef<
+  EditorType | undefined,
+  EditorPropsWithHandlers
+>((props, ref) => (
+  <Editor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />
+));
+
 export const TextEditor = () => {
+  const editorRef = useRef<EditorType>();
+  const [editorState, setEditorState] = useState<string>();
+  useEffect(() => {
+    console.log(editorRef.current?.getInstance().getMarkdown());
+  }, [editorState]);
   return (
-    <Editor
+    <EditorWithForwardedRef
+      ref={editorRef}
+      onChange={setEditorState}
       initialValue="hello react editor world!"
       previewStyle="vertical"
       height="600px"
