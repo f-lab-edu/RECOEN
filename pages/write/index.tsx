62f3cf02d9b05d437d37deba01b4a5c8f8a50d8f
yechan <noy3928@naver.com>
2022-09-17 12:31:09 +0900

    feat : create post from editor content at write page

--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
 import { TextEditor } from 'src/components/molecules';
+import axios from 'axios';
 
 const WriteContainer = () => {
   const [content, setContent] = useState<string>();
-  console.log(content);
-  return <TextEditor onChange={setContent} />;
+  const handleCreate = () => {
+    axios
+      .post('/api/article/create', {
+        content,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <button onClick={handleCreate}>생성하기</button>
+      <TextEditor onChange={setContent} />
+    </>
+  );
 };
 
 export default WriteContainer;
