6dd42aba85ae88d53a3d7aa8704704ec9709b0c4
yechan <noy3928@naver.com>
2022-09-17 12:48:56 +0900

    feat : get test content from server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
-import type { NextPage } from 'next';
+import type {
+  NextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from 'next';
+import { connectMongo } from 'src/utils/connectMongo';
+import Article from 'src/models/articleModel';
 
-const Home: NextPage = () => {
-  return <div>This is Main Page</div>;
+const Home: NextPage = ({
+  articles,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <>
+      {articles.map((article: any) => (
+        <div key={article._id}>{article.content}</div>
+      ))}
+    </>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    console.log('CONNECTING TO MONGO');
+    await connectMongo();
+    console.log('CONNECTED TO MONGO');
+
+    console.log('FETCHING DATA');
+    const articles = await Article.find();
+    console.log('FETCHED DATA');
+
+    return {
+      props: {
+        articles: JSON.parse(JSON.stringify(articles)),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
+  }
+};
