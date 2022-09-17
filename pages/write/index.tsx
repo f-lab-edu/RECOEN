9f7e0701ff85b9a8ece86c46f1e3d60f1c3711fb
yechan <noy3928@naver.com>
2022-09-17 15:05:36 +0900

    feat : make Input component for title and description

--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import { TextEditor } from 'src/components/molecules';
+import { Input } from 'src/components/atoms';
+import styled from '@emotion/styled';
 import axios from 'axios';
 
 const WriteContainer = () => {
+  const [title, setTitle] = useState<string>();
+  const [description, setDescription] = useState<string>();
   const [content, setContent] = useState<string>();
   const handleCreate = () => {
     axios
@@ -17,12 +21,22 @@ const WriteContainer = () => {
       });
   };
 
+  console.log(title, description, content);
+
   return (
     <>
       <button onClick={handleCreate}>생성하기</button>
+      <TitleInput onChange={setTitle} placeholder="제목을 입력해주세요." />
+      <DescriptionInput
+        onChange={setDescription}
+        placeholder="글에대한 설명을 입력해주세요"
+      />
       <TextEditor onChange={setContent} />
     </>
   );
 };
 
 export default WriteContainer;
+
+const TitleInput = styled(Input)``;
+const DescriptionInput = styled(Input)``;
