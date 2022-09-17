ec48587ef79eb9ae8c15cca627ee2622c057376d
yechan <noy3928@naver.com>
2022-09-17 12:31:09 +0900

    feat : create post from editor content at write page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  const createArticle = async () => {
-    const res = await fetch('api/article/create', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        content: '이것이 바로 콘텐츠',
-      }),
-    });
-
-    const data = await res.json();
-    console.log(data);
-  };
-  return <button onClick={createArticle}>생성하기</button>;
+  return <div>This is Main Page</div>;
 };
 
 export default Home;
