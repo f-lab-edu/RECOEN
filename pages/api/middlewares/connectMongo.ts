4d648857648bdf26ef4df466b59531d1f5547df6
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-24 14:48:45 +0900

    fix : use process.env

--- pages/api/middlewares/connectMongo.ts
+++ pages/api/middlewares/connectMongo.ts
@@ -1,6 +1,4 @@
 import mongoose from 'mongoose';
 
 export const connectMongo = async () =>
-  await mongoose.connect(
-    `mongodb+srv://recoen:qweasd@cluster0.twmm6ue.mongodb.net/?retryWrites=true&w=majority`,
-  );
+  await mongoose.connect(`${process.env.MONGO_URI}`);
