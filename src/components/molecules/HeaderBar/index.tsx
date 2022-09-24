0dea2c10aeae831077ed01850baac89810ce1cd0
yechan <noy3928@naver.com>
2022-09-24 19:06:28 +0900

    refactor : use HeaderBarItem in HeaderBar component

--- src/components/molecules/HeaderBar/index.tsx
+++ src/components/molecules/HeaderBar/index.tsx
@@ -1,31 +1,66 @@
 import React from 'react';
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 
+import { HeaderBarItem } from 'src/components/atoms';
+
 export const HeaderBar = () => {
+  const items = [
+    {
+      id: 'dev',
+      name: 'Dev',
+      title: '개발 아티클 리스트 페이지입니다.',
+      path: '/dev',
+    },
+    {
+      id: 'book',
+      name: 'Book',
+      title: '독후감 아티클 리스트 페이지입니다.',
+      path: '/book',
+    },
+    {
+      id: 'essay',
+      name: 'Essay',
+      title: '에세이 리스트 페이지입니다.',
+      path: '/essay',
+    },
+    {
+      id: 'quotes',
+      name: 'Quotes',
+      title: '인용구 리스트 페이지입니다.',
+      path: '/quotes',
+    },
+    {
+      id: 'about',
+      name: 'About',
+      title: '소개페이지입니다.',
+      path: '/about',
+    },
+  ];
+
+  const router = useRouter();
+
   return (
     <Container>
       <Link href="/">
         <Title>recoen.</Title>
       </Link>
-      <Wrapper>
-        <Link href="/dev">
-          <BarItem>Dev</BarItem>
-        </Link>
-        <Link href="/book">
-          <BarItem>Book</BarItem>
-        </Link>
-        <Link href="/essay">
-          <BarItem>Essay</BarItem>
-        </Link>
-        <Link href="/Quotes">
-          <BarItem>Quotes</BarItem>
-        </Link>
-        <Link href="/About">
-          <BarItem>About</BarItem>
-        </Link>
-      </Wrapper>
+      <nav>
+        <Wrapper>
+          {items.map((item) => {
+            return (
+              <HeaderBarItem
+                key={item.id}
+                title={item.title}
+                name={item.name}
+                path={item.path}
+                isActive={item.path == router.pathname}
+              />
+            );
+          })}
+        </Wrapper>
+      </nav>
     </Container>
   );
 };
@@ -45,18 +80,14 @@ const Container = styled.div`
 `;
 
 const Title = styled.a`
-  font-size: 25px;
+  font-size: 30px;
   font-weight: 600;
   color: #ffffff;
   cursor: pointer;
 `;
 
-const Wrapper = styled.nav`
+const Wrapper = styled.ul`
   display: flex;
   gap: 40px;
-`;
-
-const BarItem = styled.a`
-  color: #ffffff;
-  cursor: pointer;
+  list-style: none;
 `;
