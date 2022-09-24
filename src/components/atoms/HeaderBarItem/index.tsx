6f097255064f67ade5bf584968091b19fd49cbcd
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-24 20:58:41 +0900

    feat : add active style to HeaderBarItem

--- src/components/atoms/HeaderBarItem/index.tsx
+++ src/components/atoms/HeaderBarItem/index.tsx
@@ -8,21 +8,38 @@ interface Props {
   name: string;
 }
 
+interface StyleProps {
+  isActive: boolean;
+}
+
 export const HeaderBarItem = ({ isActive, path, title, name }: Props) => {
   console.log(isActive);
   return (
-    <Link href={path} title={title}>
+    <Link href={path} title={title} isActive={isActive}>
       <Item>{name}</Item>
     </Link>
   );
 };
 
-const Link = styled.a`
+const Link = styled.a<StyleProps>`
   cursor: pointer;
   text-decoration-line: none;
+  display: flex;
+  align-items: center;
+  position: relative;
+  ${(props) =>
+    props.isActive &&
+    `:before {
+    content: '';
+    position: absolute;
+    left: -15px;
+    background-color: #0af5cb;
+    width: 5px;
+    height: 5px;
+    border-radius: 50%;
+  }`}
 `;
 
 const Item = styled.li`
   color: #ffffff;
-  display: inline;
 `;
