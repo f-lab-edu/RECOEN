0345ae2af398136ff17a2050dd67e84336f3f6d2
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-24 12:44:00 +0900

    temp: using mongo uri directly temporary when connectMongo for testing purpose

--- pages/api/middlewares/connectMongo.ts
+++ pages/api/middlewares/connectMongo.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
 
 export const connectMongo = async () =>
-  await mongoose.connect(`${process.env.MONGO_URI}`);
+  await mongoose.connect(
+    `mongodb+srv://recoen:qweasd@cluster0.twmm6ue.mongodb.net/?retryWrites=true&w=majority`,
+  );
