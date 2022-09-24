e148263650827f32a29b892481ce22eae7b43691
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-24 18:30:32 +0900

    feat : change body properties

--- src/shared/globalStyle.ts
+++ src/shared/globalStyle.ts
@@ -2,6 +2,9 @@ import { css } from '@emotion/react';
 
 export const globalStyles = css`
   body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
     background-color: #252628;
     font-family: 'Pretendard';
   }
