a53f295d87f6baa79f96a22ce4a6d92dce67d675
yechan <noy3928@naver.com>
2022-09-25 16:45:01 +0900

    feat : using api for fetching single article

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: process.env.API_URL,
+  baseURL: process.env.API_URI,
 };
 export const client = axios.create(axiosConfig);
