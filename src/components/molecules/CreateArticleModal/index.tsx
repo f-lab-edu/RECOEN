b5178e8552783124e735285abc288cf47b70d56f
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-26 14:01:13 +0900

    feat : make image compress handler

--- src/components/molecules/CreateArticleModal/index.tsx
+++ src/components/molecules/CreateArticleModal/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Modal } from 'src/components/atoms';
 import { useS3Upload } from 'next-s3-upload';
+import { compressImage } from 'src/utils';
 
 export const CreateArticleModal = () => {
   const [imageUrl, setImageUrl] = useState<string>();
+
   const { FileInput, openFileDialog, uploadToS3 } = useS3Upload();
+
   const handleFileChange = async (file: File) => {
-    const { url } = await uploadToS3(file);
+    const compressedImage = await compressImage(file);
+    const { url } = await uploadToS3(compressedImage);
     setImageUrl(url);
   };
+
   return (
     <Modal>
       <div>
