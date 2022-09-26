790d63ecd249fd7830a6787658eeb86417ab295e
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-26 12:40:15 +0900

    feat : add upload image

--- src/components/molecules/CreateArticleModal/index.tsx
+++ src/components/molecules/CreateArticleModal/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'src/components/atoms';
+import { useS3Upload } from 'next-s3-upload';
 
 export const CreateArticleModal = () => {
+  const [imageUrl, setImageUrl] = useState<string>();
+  const { FileInput, openFileDialog, uploadToS3 } = useS3Upload();
+  const handleFileChange = async (file: File) => {
+    const { url } = await uploadToS3(file);
+    setImageUrl(url);
+  };
   return (
     <Modal>
-      <div>This is CreateArticle Modal</div>
+      <div>
+        <FileInput onChange={handleFileChange} />
+        <button onClick={openFileDialog}>Upload file</button>
+      </div>
     </Modal>
   );
 };
