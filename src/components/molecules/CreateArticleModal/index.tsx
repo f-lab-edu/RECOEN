7a080c1ebddae9be1d330527ee225f203ea3fd8e
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-27 12:46:07 +0900

    refactor : passing articleElement from write page to modal
    write 페이지 컴포넌트에선 제목, 해시태그, 컨텐츠를 작성하도록하고,
    작성하고나면 모달을 띄웁니다. 모달을 띄우고나면 모달에선 썸네일과 설명글을 입력합니다.
    모달에서 서버로 제목,해시태그,컨텐츠,썸네일이미지,설명글 을 보내도록 수정했습니다.

--- src/components/molecules/CreateArticleModal/index.tsx
+++ src/components/molecules/CreateArticleModal/index.tsx
@@ -1,13 +1,38 @@
 import React, { useState } from 'react';
-import { Modal } from 'src/components/atoms';
+import { Modal, Input } from 'src/components/atoms';
 import { ImageUpload } from 'src/components/atoms';
+import { client } from 'src/utils';
+import styled from '@emotion/styled';
 
-export const CreateArticleModal = () => {
+interface Props {
+  articleElements: {
+    title?: string;
+    hashtag?: string;
+    content?: string;
+  };
+}
+
+export const CreateArticleModal = ({ articleElements }: Props) => {
   const [imageUrl, setImageUrl] = useState<string>();
+  const [description, setDescription] = useState<string>();
+  console.log(articleElements);
+
+  const handleCreateArticle = () => {
+    client.post('/api/article', {
+      title: articleElements.title,
+      description: articleElements.hashtag, // NOTE : 서버쪽 코드 수정 후 수정하기
+      content: articleElements.content,
+    });
+  };
 
   return (
     <Modal>
-      <ImageUpload setImageUrl={setImageUrl} />
+      <>
+        <ImageUpload setImageUrl={setImageUrl} />
+        <DescriptionInput onChange={setDescription} />
+      </>
     </Modal>
   );
 };
+
+const DescriptionInput = styled(Input)``;
