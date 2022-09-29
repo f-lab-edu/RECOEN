a85de4b26b1723ea7ec7a0215f540649d12494be
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-30 00:53:50 +0900

    feat : modify Button style

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useS3Upload } from 'next-s3-upload';
 import { compressImage } from 'src/utils';
 import { usePreview } from 'src/hooks';
+import styled from '@emotion/styled';
 
 interface Props {
   setImageUrl: (e: string) => void;
@@ -22,10 +23,24 @@ export const ImageUpload = ({ setImageUrl }: Props) => {
   return (
     <div>
       <FileInput onChange={handleFileChange} />
-      <button onClick={openFileDialog}>Upload file</button>
+      {!preview && <UploadBox onClick={openFileDialog}>Upload file</UploadBox>}
       {preview && (
-        <img src={preview} alt="preview image" width="200" height="200" />
+        <Img src={preview} alt="preview image" width="200" height="200" />
       )}
     </div>
   );
 };
+
+const UploadBox = styled.button`
+  width: 350px;
+  height: 220px;
+  border: 1px solid #494c56;
+  background-color: #303236;
+  color: #494c56;
+  cursor: pointer;
+`;
+
+const Img = styled.img`
+  width: 350px;
+  height: 220px;
+`;
