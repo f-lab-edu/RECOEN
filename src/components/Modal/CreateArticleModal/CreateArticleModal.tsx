b9ae7f2c0adda1ae0badb8f3cfe84bf5ae4d335a
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-09-30 01:32:21 +0900

    feat : make Button cpnt

--- src/components/Modal/CreateArticleModal/CreateArticleModal.tsx
+++ src/components/Modal/CreateArticleModal/CreateArticleModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Modal, Input } from 'src/components/Common';
-import { ImageUpload } from 'src/components/Common';
+import { Modal, Input, Button } from 'src/components/Common';
+import { ImageUpload } from 'src/components';
 import { createArticle } from 'src/utils';
 import styled from '@emotion/styled';
 
@@ -19,18 +19,25 @@ export const CreateArticleModal = ({ articleElements }: Props) => {
   return (
     <Modal>
       <>
+        <ButtonWrapper>
+          <Button
+            label="취소"
+            buttonType="secondary"
+            onClick={() => console.log('close modal')}
+          />
+          <Button
+            label="저장"
+            buttonType="primary"
+            onClick={() =>
+              createArticle({ ...articleElements, imgUrl, description })
+            }
+          />
+        </ButtonWrapper>
         <Guide>대표이미지 선택</Guide>
         <ImageUpload setImageUrl={setImgUrl} />
         <Guide>태그를 선택하세요.</Guide>
         <DescriptionInput onChange={setDescription} />
         <Guide>설명글을 작성해주세요.</Guide>
-        <button
-          onClick={() =>
-            createArticle({ ...articleElements, imgUrl, description })
-          }
-        >
-          생성하기
-        </button>
       </>
     </Modal>
   );
@@ -46,4 +53,12 @@ const Guide = styled.h3`
   border-bottom: 1px solid #494c56;
   text-align: left;
   width: 350px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 350px;
+  display: flex;
+  justify-content: flex-end;
+  gap: 16px;
+  margin-top: 50px;
 `;
