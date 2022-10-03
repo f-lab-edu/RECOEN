c22eb914c97109bd7a227a1bec8a7c0afca69026
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-03 09:34:57 +0900

    feat : get JSON file from local folder

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,8 +1,24 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
 import { QuoteSlider } from 'src/components';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  quotesData,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(quotesData.quotes);
   return <QuoteSlider />;
 };
 
 export default Home;
+
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const filePath = path.join(process.cwd(), '__data__/quotes.json');
+  const jsonData = await fsPromises.readFile(filePath, 'utf-8');
+  const quotesData = JSON.parse(jsonData);
+
+  return {
+    props: { quotesData },
+  };
+};
