8581322834686af2d6099d09eafb5c23fcd2122d
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-04 10:04:24 +0900

    feat : set single quote size

--- src/shared/globalStyle.ts
+++ src/shared/globalStyle.ts
@@ -1,7 +1,11 @@
 import { css } from '@emotion/react';
 
 export const globalStyles = css`
+  html {
+    height: 100vh;
+  }
   body {
+    height: 100vh;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
