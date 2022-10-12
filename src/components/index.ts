b70a14f868516a167c6b634a2ebfff7cf06efe24
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-12 15:07:06 +0900

    refactor : make HeaderBar folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,8 @@ import dynamic from 'next/dynamic';
 export const TextEditor = dynamic(() => import('./TextEditor'), {
   ssr: false,
 });
+export * from './headerBar';
 export * from './article';
-export * from './HeaderBar';
 export * from './ImageUpload';
 export * from './modals';
 export * from './ui';
