d4532fbd26bf47e016ad116f3ff2c12145b5577a
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-14 17:04:09 +0900

    feat : change Title Input style
    hover나

--- src/components/Inputs/TitleInput.tsx
+++ src/components/Inputs/TitleInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import TextField from '@mui/material/TextField';
 
 interface Props {
   onChange: (value: string) => void;
@@ -47,19 +47,3 @@ export const TitleInput = ({ onChange }: Props) => {
     />
   );
 };
-
-const StyledInput = styled.input`
-  width: 100%;
-  height: 100%;
-  font-size: 18px;
-  outline: none;
-  background: #232428;
-  padding: 0px 20px;
-  color: #ffffff;
-  border: 1px solid #373a3f;
-  border-left: none;
-  box-sizing: border-box;
-  ::placeholder {
-    color: #4a4c55;
-  }
-`;
