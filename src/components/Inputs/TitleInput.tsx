e177e2283234d3325b4111a8d12378a8ca7519fa
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-14 17:04:09 +0900

    feat : change Title Input style
    hover나

--- src/components/Inputs/TitleInput.tsx
+++ src/components/Inputs/TitleInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import TextField from '@mui/material/TextField';
 
 interface Props {
   onChange: (value: string) => void;
@@ -9,23 +9,40 @@ export const TitleInput = ({ onChange }: Props) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.value);
   };
+
+  const textFieldStyle = {
+    fontSize: '20px',
+    '& .MuiOutlinedInput-notchedOutline': {
+      border: '1px solid #373a3f',
+      borderLeft: 'none',
+    },
+    '& :hover .MuiOutlinedInput-notchedOutline': {
+      border: '1px solid #373a3f !important',
+      borderLeft: 'none !important',
+    },
+    '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+      border: '1px solid #373a3f !important',
+      borderLeft: 'none !important',
+    },
+    '& label': {
+      color: '#4a4c54',
+      fontSize: '18px',
+    },
+    '& .MuiInputBase-input': {
+      color: '#ffffff',
+    },
+    '& .MuiInputBase-root': {
+      borderRadius: '0px',
+      height: '100%',
+    },
+  };
   return (
-    <StyledInput onChange={handleChange} placeholder="제목을 입력하세요." />
+    <TextField
+      onChange={handleChange}
+      label="Please enter title of this article"
+      variant="outlined"
+      fullWidth
+      sx={textFieldStyle}
+    />
   );
 };
-
-const StyledInput = styled.input`
-  width: 100%;
-  height: 100%;
-  font-size: 18px;
-  outline: none;
-  background: #232428;
-  padding: 0px 20px;
-  color: #ffffff;
-  border: 1px solid #373a3f;
-  border-left: none;
-  box-sizing: border-box;
-  ::placeholder {
-    color: #4a4c55;
-  }
-`;
