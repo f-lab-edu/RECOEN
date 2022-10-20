45c87417c77e41cc051381d9ce5db9138af671d3
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-21 00:07:05 +0900

    test : add disable type
    props를 구성해주는 부분이 가독성이 너무 별로라고 생각되는데 어떻게 개선해줄 수 있을지 고민입니다..

--- src/components/ui/Button/Button.test.tsx
+++ src/components/ui/Button/Button.test.tsx
@@ -10,17 +10,18 @@ describe('Button', () => {
     return render(<Button {...props} />);
   };
 
-  const propsWithPrimary = (primary: boolean) => {
+  const props = (primary: boolean, disabled: boolean) => {
     return {
       label: '버튼',
-      primary,
       onClick,
+      primary,
+      disabled,
     };
   };
 
   describe('label에 넘겨준 글자가', () => {
     it('화면에 보인다', () => {
-      const { getByText } = renderButton(propsWithPrimary(false));
+      const { getByText } = renderButton(props(false, false));
       expect(getByText('버튼')).toBeInTheDocument();
     });
   });
@@ -28,14 +29,14 @@ describe('Button', () => {
   describe('normal type에서', () => {
     describe('가만히 있으면', () => {
       it('폰트색은 #4a4c55이다', () => {
-        const { getByText } = renderButton(propsWithPrimary(false));
+        const { getByText } = renderButton(props(false, false));
         expect(getByText('버튼')).toHaveStyleRule('color', '#4a4c55');
       });
     });
 
     describe('호버를 하면', () => {
       it('폰트색은 #9599a0이다', () => {
-        const { getByText } = renderButton(propsWithPrimary(false));
+        const { getByText } = renderButton(props(false, false));
         const button = getByText('버튼');
         fireEvent.mouseOver(button);
 
@@ -45,7 +46,7 @@ describe('Button', () => {
 
     describe('disable 상태이면', () => {
       it('cursor가 not-allowed처리된다', () => {
-        const { getByText } = renderButton(propsWithPrimary(true));
+        const { getByText } = renderButton(props(false, true));
         expect(getByText('버튼')).toHaveStyleRule('cursor', 'not-allowed');
       });
     });
@@ -54,19 +55,19 @@ describe('Button', () => {
   describe('primary type에서', () => {
     describe('가만히 있으면', () => {
       it('폰트색은 #ffffff이다', () => {
-        const { getByText } = renderButton(propsWithPrimary(true));
+        const { getByText } = renderButton(props(true, false));
         expect(getByText('버튼')).toHaveStyleRule('color', '#ffffff');
       });
 
       it('배경색은 #3941FF이다', () => {
-        const { getByText } = renderButton(propsWithPrimary(true));
+        const { getByText } = renderButton(props(true, false));
         expect(getByText('버튼')).toHaveStyleRule('background', '#3941FF');
       });
     });
 
     describe('호버를 하면', () => {
       it('배경색은 #4E75FF이다', () => {
-        const { getByText } = renderButton(propsWithPrimary(true));
+        const { getByText } = renderButton(props(true, false));
         const button = getByText('버튼');
 
         fireEvent.mouseOver(button);
@@ -77,7 +78,7 @@ describe('Button', () => {
 
     describe('disable 상태이면', () => {
       it('cursor가 not-allowed처리된다', () => {
-        const { getByText } = renderButton(propsWithPrimary(true));
+        const { getByText } = renderButton(props(true, true));
         expect(getByText('버튼')).toHaveStyleRule('cursor', 'not-allowed');
       });
     });
