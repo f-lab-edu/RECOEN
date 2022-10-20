059f485a5e962dffc3800285be7d2dccf3cfc7e9
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-20 15:53:46 +0900

    feat : make MDX component

--- src/components/mdx/MDXComponent.tsx
+++ src/components/mdx/MDXComponent.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+import styled from '@emotion/styled';
 
 const MDXComponent = {
-  h1: (props: any) => <Typography variant="h1" {...props} />,
-  h2: (props: any) => <Typography variant="h2" {...props} mt={5} mb={5} />,
-  h3: (props: any) => <Typography variant="h3" {...props} />,
-  p: (props: any) => <Typography paragraph {...props} />,
+  h1: (props: any) => <H1 {...props} />,
+  h2: (props: any) => <H2 {...props} />,
+  h3: (props: any) => <H3 {...props} />,
+  p: (props: any) => <Paragraph {...props} />,
+  pre: (props: any) => <CodeBlock {...props} />,
 };
 
 export default MDXComponent;
+
+const H1 = styled.h1`
+  font-weight: 400;
+  font-size: 42px;
+`;
+
+const H2 = styled.h2`
+  font-weight: 400;
+  font-size: 35px;
+`;
+
+const H3 = styled.h3`
+  font-weight: 400;
+  font-size: 25px;
+`;
+
+const Paragraph = styled.p`
+  color: #9599a0;
+  font-weight: 200;
+  letter-spacing: 1px;
+  line-height: 27px;
+`;
+
+const CodeBlock = (props: any) => {
+  return (
+    <Pre>
+      <Code {...props} />
+    </Pre>
+  );
+};
+
+const Code = styled.code``;
+
+const Pre = styled.pre``;
