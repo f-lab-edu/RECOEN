fd95131cbed98d7bbe27142d0640748e8090ec57
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-20 15:53:46 +0900

    feat : make MDX component

--- src/components/mdx/DetailMDX.tsx
+++ src/components/mdx/DetailMDX.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
-
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import MDXComponent from 'src/components/mdx/MDXComponent';
-import Typography from '@mui/material/Typography';
 
 interface Props {
   title: string;
@@ -14,7 +12,7 @@ interface Props {
 const DetailMDX: React.FC<Props> = ({ title, content, time }) => {
   return (
     <Container>
-      <Typography variant="h1">{title}</Typography>
+      <Title>{title}</Title>
       <MDXRemote {...content} components={MDXComponent} />
     </Container>
   );
@@ -25,7 +23,7 @@ export default DetailMDX;
 const Container = styled.main`
   position: absolute;
   width: 1200px;
-  padding: 50px 100px;
+  padding: 50px 200px;
   margin: 0 auto;
   background-color: #252628;
   margin-left: auto;
@@ -34,6 +32,10 @@ const Container = styled.main`
   right: 0;
   top: 340px;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  box-sizing: border-box;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 45px;
+  line-height: 65px;
+`;
---
+++ src/components/mdx/DetailTitle.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const DetailTitle = () => {
+  return <div></div>;
+};
+
+export default DetailTitle;
