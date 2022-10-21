d670797429179a3017c9a0d61b3eac56826a2e15
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-21 15:36:29 +0900

    refactor : move write page test directory

--- pages/write/writepage.test.tsx
+++ pages/write/writepage.test.tsx
@@ -2,12 +2,10 @@ import { render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { RecoilRoot } from 'recoil';
 
-import WritePage from 'pages/write/index.page';
+import WritePage from './index.page';
 
 jest.mock('src/components/TextEditor');
 jest.mock('src/utils');
-
-// NOTE : 작성하기 페이지의 책임에 대한 고민. 작성하기 페이지에 헤더 컴포넌트가 나오긴 하지만, 작성하기를 하는 책임이 헤더바 컴포넌트에게 가있는 상황. 이것이 맞는 것일까?
 
 describe('WritePage', () => {
   const renderWritePage = () =>
