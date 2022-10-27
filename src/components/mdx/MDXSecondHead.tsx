da2ad19ced7b5e5d988afde91d413197d1f5304e
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-27 15:21:10 +0900

    feat : change font-size

--- src/components/mdx/MDXSecondHead.tsx
+++ src/components/mdx/MDXSecondHead.tsx
@@ -5,17 +5,18 @@ import styled from '@emotion/styled';
 const MDXSecondHead: React.FC<any> = (props) => {
   const fragment = makeTitleFragment(props.children);
   return (
-    <H1 id={fragment}>
+    <H2 id={fragment}>
       <a href={`#${fragment}`} {...props} />
-    </H1>
+    </H2>
   );
 };
 
 export default MDXSecondHead;
 
-const H1 = styled.h1`
+const H2 = styled.h2`
   font-weight: 400;
-  font-size: 42px;
+  font-size: 30px;
+  margin: 35px 0px;
   & a {
     color: #57a1f8;
     text-decoration: none;
