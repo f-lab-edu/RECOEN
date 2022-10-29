c520d59462942909da3659e137616f7574786721
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-30 02:57:01 +0900

    feat : create post by using recoil state

--- src/hooks/useCreatArticle.ts
+++ src/hooks/useCreatArticle.ts
@@ -109,9 +109,25 @@ export const useContent = () => {
 };
 
 export const useTags = () => {
-  const [articleElements, setArticleElements] = useRecoilState(ArticleStates);
+  const [articleElements, setArticleElements] = useRecoilState(articleStates);
   const setTags = (tags: string[]) => {
     setArticleElements({ ...articleElements, tags });
   };
   return { tags: articleElements.tags, setTags };
+};
+
+export const useDescription = () => {
+  const [articleElements, setArticleElements] = useRecoilState(articleStates);
+  const setDesc = (description: string) => {
+    setArticleElements({ ...articleElements, description });
+  };
+  return { setDesc };
+};
+
+export const useImageUrl = () => {
+  const [articleElements, setArticleElements] = useRecoilState(articleStates);
+  const setUrl = (imgUrl: string) => {
+    setArticleElements({ ...articleElements, imgUrl });
+  };
+  return { setUrl };
 };
