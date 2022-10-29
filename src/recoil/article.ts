a8341e8975d0b602026917b4971e81b2ba455f4c
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-30 01:14:00 +0900

    feat : make article recoil state

---
+++ src/recoil/article.ts
@@ -0,0 +1,12 @@
+import { atom } from 'recoil';
+
+export const ArticleStates = atom({
+  key: 'articleStates',
+  default: {
+    title: '',
+    content: '',
+    tags: [],
+    description: '',
+    imgUrl: '',
+  },
+});
