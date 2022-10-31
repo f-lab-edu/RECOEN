675733f302b84561a95da163da9e28dc0b0fee2b
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-10-31 22:07:18 +0900

    feat : make update page

---
+++ pages/write/[id].page.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import WritePage from './index.page';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { connectMongo } from 'pages/api/middlewares/connectMongo';
+
+import { useSetRecoilState, useResetRecoilState, useRecoilValue } from 'recoil';
+import { articleStates } from 'src/recoil/article';
+
+import ArticleModel from 'pages/api/models/articleModel';
+
+const UpdatePage = ({
+  article,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const recoilArticle = useRecoilValue(articleStates);
+  const setArticle = useSetRecoilState(articleStates);
+  const resetDetailStates = useResetRecoilState(articleStates);
+  console.log('recoilArticle :', recoilArticle);
+
+  useEffect(() => {
+    const articleState = {
+      _id: article._id,
+      title: article.title,
+      content: article.content,
+      imgUrl: article.imgUrl,
+      description: article.description,
+      tags: article.tags,
+    };
+    setArticle(articleState);
+
+    return () => resetDetailStates();
+  }, []);
+
+  return <WritePage />;
+};
+
+export default UpdatePage;
+
+interface IPrams extends ParsedUrlQuery {
+  id: string;
+}
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  await connectMongo();
+  const { id } = context.params as IPrams;
+  const res = await ArticleModel.findById(id);
+  const article = JSON.parse(JSON.stringify(res));
+
+  return { props: { article } };
+};
