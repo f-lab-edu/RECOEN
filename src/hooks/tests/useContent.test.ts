1ad7e5a2dad499aa0e1cc53398227d9b2cc93f52
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-11-01 01:55:54 +0900

    test : useContent

---
+++ src/hooks/tests/useContent.test.ts
@@ -0,0 +1,28 @@
+import { renderHook, act } from '@testing-library/react';
+import { useContent } from '../useCreatArticle';
+import { RecoilRoot } from 'recoil';
+
+describe('useContent', () => {
+  describe('초기값은', () => {
+    it('빈 값이다', () => {
+      const { result } = renderHook(() => useContent(), {
+        wrapper: RecoilRoot,
+      });
+      expect(result.current.content).toBe('');
+    });
+  });
+
+  describe('setMarkDown을 통해서', () => {
+    it('content 값을 변경시킬 수 있다', () => {
+      const { result } = renderHook(() => useContent(), {
+        wrapper: RecoilRoot,
+      });
+
+      act(() => {
+        result.current.setMarkDown('# 컨텐츠');
+      });
+
+      expect(result.current.content).toBe('# 컨텐츠');
+    });
+  });
+});
