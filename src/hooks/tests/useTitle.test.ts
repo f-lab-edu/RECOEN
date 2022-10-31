064cc7c482cd127ce978920e5003f23ca2ae8f2c
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-11-01 01:53:00 +0900

    test : useTitle

--- src/hooks/tests/useTitle.test.ts
+++ src/hooks/tests/useTitle.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useTitle } from '../useCreatArticle';
 import { RecoilRoot } from 'recoil';
 
@@ -7,6 +7,23 @@ describe('useTitle', () => {
     it('빈 값이다', () => {
       const { result } = renderHook(() => useTitle(), { wrapper: RecoilRoot });
       expect(result.current.articleElements.title).toBe('');
+    });
+  });
+
+  describe('setArticleTitle을 통해서', () => {
+    it('articleElements.title 값을 변경시킬 수 있다', () => {
+      const { result } = renderHook(() => useTitle(), { wrapper: RecoilRoot });
+
+      act(() => {
+        result.current.setArticleTitle({
+          target: {
+            name: 'text',
+            value: '제목',
+          },
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.articleElements.title).toBe('제목');
     });
   });
 });
