9256a4ea6c85a53d2a79dba55b9f4964d6dc0318
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-11-08 18:16:29 +0900

    feat : make Modal component

--- src/components/modals/Modal/Modal.tsx
+++ src/components/modals/Modal/Modal.tsx
@@ -1,72 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import styled from '@emotion/styled';
+import React from 'react';
 
-interface ModalProps {
-  children: React.ReactElement;
-  handleOpenModal: () => void;
-  right?: boolean;
-}
+import { useRecoilValue } from 'recoil';
+import { modalState } from 'src/recoil/modal';
 
-const Modal = ({ children, handleOpenModal, right }: ModalProps) => {
-  const [container, setContainer] = useState<HTMLElement | null>(null);
+import CreateArticleModal from 'src/components/modals/CreateArticleModal/CreateArticleModal';
+import LoginModal from 'src/components/modals/LoginModal/LoginModal';
 
-  useEffect(() => {
-    setContainer(document.getElementById('modal_root'));
-  }, []);
+const Modal = () => {
+  const modalType = useRecoilValue(modalState);
 
-  if (!container) return null;
+  const modal = {
+    CREATE_ARTICLE: <CreateArticleModal />,
+    LOGIN: <LoginModal />,
+  };
 
-  return ReactDOM.createPortal(
-    <>
-      <Overlay onClick={handleOpenModal} data-testid="overlay" />
-      <Box data-testid="modal" right={right}>
-        {children}
-      </Box>
-    </>,
-    container,
-  );
+  if (!modalType) return null;
+
+  return <>{modal[modalType]}</>;
 };
 
 export default Modal;
-
-interface StyleProps {
-  right?: boolean;
-}
-
-const Overlay = styled.div`
-  height: 100vh;
-  width: 100vw;
-  left: 0;
-  top: 0;
-  background: #161718;
-  opacity: 70%;
-  backdrop-filter: saturate(180%) blur(20px);
-  -webkit-backdrop-filter: saturate(180%) blur(20px);
-  position: fixed;
-  z-index: 100;
-  cursor: pointer;
-`;
-
-const rightStyle = `
-right: 0%;
-top: 50%;
-transform: translate(0%, -50%);
-`;
-
-const centerStyle = `
-left: 50%;
-top: 50%;
-transform: translate(-50%, -50%);
-`;
-
-const Box = styled.div<StyleProps>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  position: fixed;
-  z-index: 101;
-  box-sizing: border-box;
-  ${({ right }) => (right ? rightStyle : centerStyle)}
-`;
