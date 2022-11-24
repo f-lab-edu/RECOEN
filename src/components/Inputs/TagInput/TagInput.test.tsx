35b3c7ce730b617fad33c112acbdf51c2aaf3499
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-11-24 16:38:37 +0900

    test : fix test error

--- src/components/Inputs/TagInput/TagInput.test.tsx
+++ src/components/Inputs/TagInput/TagInput.test.tsx
@@ -5,6 +5,8 @@ import RecoilObserver from 'src/components/RecoilObserver';
 import { RecoilRoot } from 'recoil';
 import { articleState } from 'src/recoil/article';
 
+import { useArticleElement } from 'src/hooks/useHandleArticle';
+
 expect.extend(matchers);
 
 describe('TagInput', () => {
@@ -13,7 +15,7 @@ describe('TagInput', () => {
     render(
       <RecoilRoot>
         <RecoilObserver node={articleState} onChange={onChange} />
-        <TagInput />
+        <TagInput useArticleElement={useArticleElement} />
       </RecoilRoot>,
     );
 
