09729c44309a6ca49d818af192a9403c428ce326
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-12-01 18:47:43 +0900

    feat : add timestamp to article

--- pages/api/models/articleModel.ts
+++ pages/api/models/articleModel.ts
@@ -1,12 +1,15 @@
 import { Schema, model, models } from 'mongoose';
 
-const articleSchema = new Schema({
-  title: String,
-  description: String,
-  tags: [String],
-  content: String,
-  imgUrl: String,
-});
+const articleSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    tags: [String],
+    content: String,
+    imgUrl: String,
+  },
+  { timestamps: true },
+);
 
 const ArticleModel = models.Article || model('Article', articleSchema);
 
