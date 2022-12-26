a277ec68260359123674ca47ddc592d29eee37c8
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-12-26 21:33:21 +0900

    test : test Article component

--- src/components/article/Article/Article.test.tsx
+++ src/components/article/Article/Article.test.tsx
@@ -1,25 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Article from './Article';
+import { viewArticle } from 'src/fixtures';
 
 describe('<Article/>', () => {
   it('should render correctly', () => {
-    const props = {
-      title: '리액트 fiber에 대해서 알아보자',
-      description: '리액트 fiber에 대해서 설명하는 글입니다',
-      path: '123',
-      imgUrl:
-        'http://localhost:3000/_next/image?url=https%3A%2F%2Frecoen.s3.ap-northeast-2.amazonaws.com%2Fnext-s3-uploads%2F34606707-2b1e-4f0f-ac8b-d4d937a240d4%2Fsumner-mahaffey-7Y0NshQLohk-unsplash.jpg&w=3840&q=75',
-      hashtag: '리액트',
-      blurDataURL:
-        "'http://localhost:3000/_next/image?url=https%3A%2F%2Frecoen.s3.ap-northeast-2.amazonaws.com%2Fnext-s3-uploads%2F34606707-2b1e-4f0f-ac8b-d4d937a240d4%2Fsumner-mahaffey-7Y0NshQLohk-unsplash.jpg&w=3840&q=75'",
-    };
     render(
       <Article
-        title={props.title}
-        description={props.description}
-        path={props.path}
-        imgUrl={props.imgUrl}
-        blurDataURL={props.blurDataURL}
+        title={viewArticle.title}
+        description={viewArticle.description}
+        path="/book/[id]"
+        imgUrl={viewArticle.imgUrl}
+        blurDataURL={viewArticle.blurDataURL}
+        createdAt={viewArticle.createdAt}
+        type={viewArticle.category}
       />,
     );
 
