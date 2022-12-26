153cd0cf32a64224899abef7cc24e234ab660663
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-12-26 20:42:31 +0900

    fix : context should be loaded in the text-editor when enter the update page

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import { Editor } from '@toast-ui/react-editor';
@@ -13,7 +13,7 @@ interface Props {
 const TextEditor: React.FC<Props> = ({ useArticleElement }) => {
   const editorRef = useRef<Editor>();
   const { uploadToS3 } = useS3Upload();
-  const { setArticleElement } = useArticleElement();
+  const { articleElements, setArticleElement } = useArticleElement();
 
   const handleChange = () => {
     if (!editorRef.current) return;
@@ -28,6 +28,11 @@ const TextEditor: React.FC<Props> = ({ useArticleElement }) => {
     const { url } = await uploadToS3(blob);
     callback(url, 'alt text');
   };
+
+  useEffect(() => {
+    if (!editorRef.current) return;
+    editorRef.current.getInstance().setMarkdown(articleElements.content);
+  }, []);
 
   return (
     <Editor
