73ef30448be3a4907ac8ac52a1d1ed463fc93715
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-02 17:38:36 +0900

    feat : write page on mobile and tablet devices

--- pages/write/index.page.tsx
+++ pages/write/index.page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import WritePageContainer from 'src/components/container/WritePageContainer';
+import MobileWritePage from 'src/components/container/MobileWritePage';
+import { useResolution } from 'src/hooks';
 
 const WritePage = () => {
-  return <WritePageContainer />;
+  const resolution = useResolution();
+  if (resolution == 'DESKTOP') return <WritePageContainer />;
+  return <MobileWritePage />;
 };
 
 export default WritePage;
