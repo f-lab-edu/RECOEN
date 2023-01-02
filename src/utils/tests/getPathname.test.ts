201a6475f77266744a95f73f846ea71948104651
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-02 18:00:27 +0900

    test : make router mock data

--- src/utils/tests/getPathname.test.ts
+++ src/utils/tests/getPathname.test.ts
@@ -1,26 +1,19 @@
 import { getPathname } from '../getPathname';
+import { mockRouter } from 'src/fixtures';
 
 describe('getPathname', () => {
-  const router = {
-    pathname: '/book',
-    query: {},
-  };
-
-  const router2 = {
-    pathname: '/book/[id]',
-    query: { id: 'asdasdasd' },
-  };
   context('pathname에 [id]가 포함되어 있지 않다면', () => {
     it('pathname을 그대로 반환한다.', () => {
-      expect(getPathname(router.pathname)(router.query)).toBe('/book');
+      mockRouter.pathname = '/book';
+      expect(getPathname(mockRouter)).toBe('/book');
     });
   });
 
   context('pathname에 [id]가 포함되어 있다면', () => {
     it('query id와 조합된 pathname을 반환한다.', () => {
-      expect(getPathname(router2.pathname)(router2.query)).toBe(
-        '/book/asdasdasd',
-      );
+      mockRouter.pathname = '/book/[id]';
+      mockRouter.query.id = 'asdasdasd';
+      expect(getPathname(mockRouter)).toBe('/book/asdasdasd');
     });
   });
 });
