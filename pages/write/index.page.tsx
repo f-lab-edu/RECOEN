d3df1f6bc2f84be2497b36f0763012be5b97445e
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-16 15:31:46 +0900

    feat : using session

--- pages/write/index.page.tsx
+++ pages/write/index.page.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useSession } from 'next-auth/react';
 
 import WritePageContainer from 'src/components/container/WritePageContainer';
 import MobileWritePage from 'src/components/container/MobileWritePage';
 import { useDetectResolution } from 'src/hooks';
 
+import { useHandleOpenModal } from 'src/hooks/useHandleOpenModal';
+
 const WritePage = () => {
+  const { data: session, status } = useSession();
+
+  const handleOpenModal = useHandleOpenModal();
+  if (status === 'unauthenticated') handleOpenModal('UNAUTH');
+
   const resolution = useDetectResolution();
   if (resolution == 'DESKTOP') return <WritePageContainer />;
   return <MobileWritePage />;
