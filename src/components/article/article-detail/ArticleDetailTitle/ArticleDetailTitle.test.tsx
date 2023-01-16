e2187c6b7d4b18a36c504d539306ee2552a5e216
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-16 17:07:46 +0900

    test : ArticleDetailTitle

--- src/components/article/article-detail/ArticleDetailTitle/ArticleDetailTitle.test.tsx
+++ src/components/article/article-detail/ArticleDetailTitle/ArticleDetailTitle.test.tsx
@@ -1,9 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { RecoilRoot } from 'recoil';
 
 import ArticleDetailTitle from './ArticleDetailTitle';
 import { article } from 'src/fixtures';
+import { mockUseSession } from 'src/utils/test-utils';
+
+jest.mock('next-auth/react');
 
 describe('<ArticleDetailTitle/>', () => {
   const renderArticleDetailTitle = () => {
@@ -14,11 +17,24 @@ describe('<ArticleDetailTitle/>', () => {
     );
   };
 
+  beforeEach(() => mockUseSession({ isLogin: false, isAdmin: false }));
+
   it('should render correctly', () => {
     const { getByText } = renderArticleDetailTitle();
 
     expect(getByText('리액트 fiber에 대해서 알아보자')).toBeInTheDocument();
     expect(getByText('태그1')).toBeInTheDocument();
     expect(getByText('2022.12.26')).toBeInTheDocument();
+  });
+
+  context('관리자 계정일 때,', () => {
+    it('수정, 삭제 버튼이 나온다', () => {
+      mockUseSession({ isLogin: true, isAdmin: true });
+
+      const { getByText } = renderArticleDetailTitle();
+
+      expect(getByText('수정')).toBeInTheDocument();
+      expect(getByText('삭제')).toBeInTheDocument();
+    });
   });
 });
