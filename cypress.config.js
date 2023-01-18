626d3b3b2fd66bcf7976333e09e75079befbc5b8
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-18 18:49:22 +0900

    chore : remove unused config code

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require('cypress');
-const { GoogleSocialLogin } = require('cypress-social-logins').plugins;
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     viewportHeight: 800,
     viewportWidth: 1280,
-    setupNodeEvents(on, config) {
-      on('task', {
-        GoogleSocialLogin: GoogleSocialLogin,
-      });
-    },
   },
 });
