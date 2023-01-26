d668513e26871508f88220f453ca6433a181eb26
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-26 16:29:19 +0900

    fix : unmatched file dir

--- pages/server-sitemap-index.xml/index.page.tsx
+++ pages/server-sitemap-index.xml/index.page.tsx
@@ -1,15 +1,19 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap } from 'next-sitemap';
-import ProgrammingArticleModel from 'pages/api/models/programmingArticleModel';
-import BookArticleModel from 'pages/api/models/bookArticleModel';
+import ArticleCollectionModel from 'pages/api/models/articleCollectionModel';
 import { connectMongo } from 'pages/api/middlewares/connectMongo';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   await connectMongo();
-  const programmingArticleIds = await ProgrammingArticleModel.find().select(
-    '_id',
-  );
-  const bookArticleIds = await BookArticleModel.find().select('_id');
+
+  const programmingArticleIds = await ArticleCollectionModel.find({
+    category: 'programming',
+  }).select('_id');
+
+  const bookArticleIds = await ArticleCollectionModel.find({
+    category: 'book',
+  }).select('_id');
+
   const lastmod = new Date().toISOString();
 
   const AbsoluteFields = [
