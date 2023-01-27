46052083fe96c983a851c13a8ac59c52d0823444
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-27 23:09:34 +0900

    refactor : make useSettingDetailPage

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,3 +5,4 @@ export * from './useViewportWidth';
 export * from './useArticleElement';
 export * from './article/useResolveSaveArticleHandler';
 export * from './article/useHandleDelete';
+export * from './detail-page/useSettingDetailPage';
