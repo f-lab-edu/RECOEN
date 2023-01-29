1b8c7ac34f4939332476cf36f40ef59ca749683f
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-01-29 22:36:54 +0900

    fix : remove revalidate

--- pages/programming/index.page.tsx
+++ pages/programming/index.page.tsx
@@ -52,7 +52,6 @@ export const getStaticProps: GetStaticProps = async () => {
         articles: articlesWithBlurURL,
         tags,
       },
-      revalidate: 10, // In seconds
     };
   } catch (error) {
     console.log(error);
