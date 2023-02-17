7e79fe0c3dc0a1e976f54e2efde7f2430749feb8
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-17 16:34:25 +0900

    refactor : refactor getPathname with ramdajs

--- src/utils/getPathname.ts
+++ src/utils/getPathname.ts
@@ -1,8 +1,9 @@
+import prop from 'ramda/src/prop';
+import ifElse from 'ramda/src/ifElse';
 import isEmpty from 'ramda/src/isEmpty';
 import { NextRouter } from 'next/router';
 
 const makePathname = (router: NextRouter) =>
   router.pathname.replace(/\[id\]/g, router.query.id as string);
 
-export const getPathname = (router: NextRouter) =>
-  isEmpty(router.query) ? router.pathname : makePathname(router);
+export const getPathname = ifElse(isEmpty, prop('pathname'), makePathname);
