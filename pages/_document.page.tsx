e1f9a8f37f2362ad7720ad3cb616b14713f21d0e
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-20 14:42:24 +0900

    feat : remove blocking element

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -3,21 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <link
-          rel="stylesheet"
-          as="style"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard-dynamic-subset.css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
-        ></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
