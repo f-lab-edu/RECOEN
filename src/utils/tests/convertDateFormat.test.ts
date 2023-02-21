d12ed18a0478f97df9be933702728bafd361bb04
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-21 10:52:31 +0900

    test : convertDateFormat

--- src/utils/tests/convertDateFormat.test.ts
+++ src/utils/tests/convertDateFormat.test.ts
@@ -6,4 +6,10 @@ describe('convertDateFormat', () => {
   it('-로 들어온 날짜 데이터를 .로 바꿔준다.', () => {
     expect(convertDateFormat(createAt)).toBe('2022.12.26');
   });
+
+  context('date가 없을 때', () => {
+    it('undefined를 반환한다.', () => {
+      expect(convertDateFormat(undefined)).toBe(undefined);
+    });
+  });
 });
